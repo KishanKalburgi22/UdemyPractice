105bd3f753ec0be59206edb3230cc8a26f207615
Kishan_KK <kishankalburgi22@gmail.com>
2022-07-18 22:19:54 +0530

    Section - 2 Coding challenge

---
+++ Java11/src/codingchallengesec4/DecimalComparator.java
@@ -0,0 +1,15 @@
+package codingchallengesec4;
+
+public class DecimalComparator {
+
+    public static void main(String[] args) {
+        System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+        System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.176));
+        System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0));
+        System.out.println(areEqualByThreeDecimalPlaces(-3.123, 3.123));
+    }
+
+    public static boolean areEqualByThreeDecimalPlaces(double v1, double v2) {
+        return (int) (v1 * 1000) == (int) (v2 * 1000);
+    }
+}
